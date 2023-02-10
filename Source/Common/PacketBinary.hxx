c537d3dddccf93e4f51b0da9dcc37bb0bc62369a
Rayyan Khan <117920879+Xlangk@users.noreply.github.com>
2023-02-10 23:02:17 +0000

    Fix: PacketBinary subtraction fail negative bits

--- Source/Common/PacketBinary.hxx
+++ Source/Common/PacketBinary.hxx
@@ -4,6 +4,32 @@
 
 namespace Emulator {
 /**
+ * Flags object, this can be passed to PacketBinary for manipulation and see effective
+ * flags.
+ */
+struct ALUFlags {
+    /**
+     * This flag is set when the result of the operation is negative.
+     */
+    bool negative = false;
+
+    /**
+     * This flag is set when the result of the operation is zero.
+     */
+    bool zero = false;
+
+    /**
+     * This flag is set when the result of the operation is positive.
+     */
+    bool positive = false;
+
+    /**
+     * This flag is set when the result of the operation is overflow.
+     */
+    bool overflow = false;
+};
+
+/**
  * This is a class that manipulates binary data in packets. It can add values, subtract, and 
  * manipulate the binary data in packets. It can manipulate data beyond the bit capabilities of
  * the processor. Larger bits of binary must be on the left, smaller bits on the right. Any new bits that are added must
@@ -13,6 +39,8 @@ class PacketBinary {
 public:
     /**
      * This packet to decimal converter can convert binary numbers up to 64 bits.
+     * @param packet The packet to convert to decimal.
+     * @param packetSize The size of the packet, or bit width of the packet.
      */
     static size_t packetToDec(bool *packet, size_t packetSize) {
         if (packetSize > 64)
@@ -30,8 +58,15 @@ public:
      * This binary adder adds numbers without the 64 bit limit. The 64 limit exists on the fast conversion due 
      * to the address bit width. This one does not write any decimal values to 64 bit variables. So this function
      * can handle binary numbers with more than 64 bit numbers.
+     * @param packetA The first packet to add.
+     * @param packetASize The size of the first packet.
+     * @param packetB The second packet to add.
+     * @param packetBSize The size of the second packet.
+     * @param outputPacketSize The size of the output packet or the size of the largest packet.
+     * @param flags The flags object to set the flags on.
+     * @return The output packet.
      */
-    static bool *addBinary(bool *packetA, size_t packetASize, bool *packetB, size_t packetBSize, size_t outputPacketSize) {
+    static bool *addBinary(bool *packetA, size_t packetASize, bool *packetB, size_t packetBSize, size_t outputPacketSize, ALUFlags *flags) {
         size_t cary = 0;
         bool *packetC = new bool[outputPacketSize];
 
@@ -76,16 +111,74 @@ public:
             }
         }
 
+        if (cary > 0)
+            flags->overflow = true;
+        bool allZero = true;
+        for (size_t i = 0; i < outputPacketSize; i++)
+            if (packetC[i] == 1) allZero = false;
+        if (allZero) flags->zero = true;
+        else flags->positive = true;
+
         return packetC;
+    }
+
+    /**
+     * This function converts a decimal number to a binary packet.
+     * @param dec The decimal number to convert.
+     * @param packetSize The size of the packet.
+     * @return The binary packet.
+     */
+    static bool *decToPacket(size_t dec, size_t packetSize) {
+        if (packetSize > 64)
+            throw std::runtime_error("Packet size is too large. Packet size must be less than or equal to 64.");
+        bool *packet = new bool[packetSize];
+        for (size_t i = 0; i < packetSize; i++)
+            packet[i] = 0;
+        for (size_t i = 0; i < packetSize; i++) {
+            if (dec % 2 == 1)
+                packet[packetSize - i - 1] = 1;
+            dec /= 2;
+        }
+        return packet;
+    }
+
+    /**
+     * Compare two binary packets. See if packet A is larger than packet B.
+     * @param packetA The first packet to compare.
+     * @param packetASize The size of the first packet.
+     * @param packetB The second packet to compare.
+     * @param packetBSize The size of the second packet.
+     * @param flags The flags object to set the flags on.
+     * @return Whether packet A is larger than packet B is.
+     */
+    static bool compareBinarySize(bool *packetA, size_t packetASize, bool *packetB, size_t packetBSize, ALUFlags *flags) {
+        for (size_t i = 0; i < packetASize; i++)
+            if (packetA[i] == 1 && packetB[i] == 0) {
+                flags->positive = true;
+                return true;
+            } else if (packetA[i] == 0 && packetB[i] == 1) {
+                flags->negative = true;
+                return false;
+            }
+
+        flags->zero = true;
+        return false;
     }
 
     /**
      * This binary subtractor subtracts numbers without the 64 bit limit. The 64 limit exists on the fast conversion due
      * to the address bit width. This one does not write any decimal values to 64 bit variables. So this function
      * can handle binary numbers with more than 64 bit numbers.
+     * @param packetA The first packet to subtract.
+     * @param packetASize The size of the first packet.
+     * @param packetB The second packet to subtract.
+     * @param packetBSize The size of the second packet.
+     * @param outputPacketSize The size of the output packet or the size of the largest packet.
+     * @param flags The flags object to set the flags on.
+     * @return The output packet.
      */
-    static bool *subtractBinary(bool *packetA, size_t packetASize, bool *packetB, size_t packetBSize, size_t outputPacketSize) {
-        size_t cary = 0;
+    static bool *subtractBinary(bool *packetA, size_t packetASize, bool *packetB, size_t packetBSize, size_t outputPacketSize, ALUFlags *flags) {
+        size_t cary = 0;        
         bool *packetC = new bool[outputPacketSize];
 
         if (packetASize > outputPacketSize || packetBSize > outputPacketSize)
@@ -129,24 +222,17 @@ public:
             }
         }
 
-        return packetC;
-    }
+        if (cary > 0)
+            flags->overflow = true;
+        bool allZero = true;
+        for (size_t i = 0; i < outputPacketSize; i++)
+            if (packetC[i] == 1) allZero = false;
+        if (allZero) flags->zero = true;
 
-    /**
-     * This function converts a decimal number to a binary packet.
-     */
-    static bool *decToPacket(size_t dec, size_t packetSize) {
-        if (packetSize > 64)
-            throw std::runtime_error("Packet size is too large. Packet size must be less than or equal to 64.");
-        bool *packet = new bool[packetSize];
-        for (size_t i = 0; i < packetSize; i++)
-            packet[i] = 0;
-        for (size_t i = 0; i < packetSize; i++) {
-            if (dec % 2 == 1)
-                packet[packetSize - i - 1] = 1;
-            dec /= 2;
-        }
-        return packet;
+        bool largerPacket = compareBinarySize(packetA, packetASize, packetB, packetBSize, flags);
+        if (largerPacket) flags->positive = true;
+        else flags->negative = true;
+        return packetC;
     }
 };
 }
--- Source/Main.cxx
+++ Source/Main.cxx
@@ -1,11 +1,9 @@
 #include "Memory/MemoryChip.hxx"
 #include "Common/PacketBinary.hxx"
 
+using namespace Emulator;
+
 int main() {
-    std::cout << "Hello World!" << std::endl;
-    bool *dec = Emulator::PacketBinary::decToPacket(255, 8);
-    for (size_t i = 0; i < 8; i++)
-        std::cout << dec[i];
-    std::cout << std::endl;
+    
     return 0;
 }
