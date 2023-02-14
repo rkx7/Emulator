55904e7c3d193684d0a4c7f7566fa0403b79ab42
Rayyan Khan <117920879+Xlangk@users.noreply.github.com>
2023-02-14 13:14:09 +0000

    Feat: Added file reading access as stream

--- Source/Main.cxx
+++ Source/Main.cxx
@@ -1,9 +1,17 @@
 #include "Memory/MemoryChip.hxx"
 #include "Common/PacketBinary.hxx"
+#include "Platform/Host/File.hxx"
 
 using namespace Emulator;
 
 int main() {
-    
+    File file(File::combineCWDPath("../abxyz"));
+    std::cout << File::combineCWDPath("abxyz") << std::endl;
+
+    file.readBytes([](bool *byte) {
+        for (size_t i = 0; i < 8; i++)
+            std::cout << byte[i];
+        std::cout << std::endl;
+    });
     return 0;
 }
--- Source/Platform/Host/File.hxx
+++ Source/Platform/Host/File.hxx
@@ -1,9 +1,12 @@
 #pragma once
 #include <string>
+#include <functional>
 #ifdef _WIN64
 #include <Windows.h>
 #elif __linux__
 #include <unistd.h>
+#include <fcntl.h>
+#include <linux/limits.h>
 #endif
 
 namespace Emulator {
@@ -15,11 +18,11 @@ private:
     size_t m_sourceHandle;
 
 public:
-    explicit File(const char *sourcePath) {
+    explicit File(const std::string &sourcePath) {
         #ifdef _WIN64
-        m_sourceHandle = (size_t)CreateFile(sourcePath, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+        m_sourceHandle = (size_t)CreateFile(sourcePath.c_str(), GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
         #elif __linux__
-        m_sourceHandle = (size_t)open(sourcePath, O_RDONLY);
+        m_sourceHandle = (size_t)open(sourcePath.c_str(), O_RDONLY);
         #endif
     }
 
@@ -38,14 +41,50 @@ public:
 
     static std::string combineCWDPath(const std::string &suffixPath) {
         std::string cwd = accessCWD();
-        std::string combinedPath = cwd + "\\" + suffixPath;
+        std::string combinedPath = cwd + "/" + suffixPath;
         return combinedPath;
+    }
+
+    void flipBinary(bool *binary, size_t binarySize) {
+        bool *flippedBinary = new bool[binarySize];
+        for (size_t i = 0; i < binarySize; i++)
+            flippedBinary[i] = binary[binarySize - i - 1];
+        for (size_t i = 0; i < binarySize; i++)
+            binary[i] = flippedBinary[i];
+        delete[] flippedBinary;
     }
 
     /**
      * Access bytes from the file. This function is stream based, data could be extremely massive and could
      * damage the operating state of the host. The manufactured processor is expected to run near few TB of ram.
-     * @
+     * @param byteCallback The callback function to access the bytes.
      */
+    void readBytes(std::function<void(bool *byte)> byteCallback) {
+        // Read the file in 8 bit chunks, if the chunk is as an 8 bit number, convert it to binary and pass it to the callback.
+        #ifdef _WIN64
+        DWORD bytesRead;
+        char *byte = new char[1];
+        while (ReadFile((HANDLE)m_sourceHandle, byte, 1, &bytesRead, NULL) && bytesRead > 0) {
+            bool *byteBin = new bool[8];
+            for (size_t i = 0; i < 8; i++)
+                byteBin[i] = (byte[0] >> i) & 1;
+            flipBinary(byteBin, 8);
+            byteCallback(byteBin);
+            delete[] byteBin;
+        }
+        delete[] byte;
+        #elif __linux__
+        char *byte = new char[1];
+        while (read(m_sourceHandle, byte, 1) > 0) {
+            bool *byteBin = new bool[8];
+            for (size_t i = 0; i < 8; i++)
+                byteBin[i] = (byte[0] >> i) & 1;
+            flipBinary(byteBin, 8);
+            byteCallback(byteBin);
+            delete[] byteBin;
+        }
+        delete[] byte;
+        #endif
+    }
 };
 }
